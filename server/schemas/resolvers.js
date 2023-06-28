d6685e7963de5a21970ebc794ba73a9d710ec0bb
Josh Mol <joshua.molyneaux@live.com>
2023-06-28 18:53:40 -0400

    Adding updates to schemas for seeding database to work and able to query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,27 @@ const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
-
+  Query: {
+    users: async () => {
+      return User.find().populate('properties');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('properties');
+    },
+    properties: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Property.find(params);
+    },
+    property: async (parent, { propertyId }) => {
+      return Property.findOne({ _id: propertyId });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('properties');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
   // Mutation
   Mutation: {
     addUser: async (parent, { firstName, lastName, username, email, password }) => {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,18 +28,30 @@ type Property {
     reviews: [Review]!
 }
 
+type Review {
+  _id: ID
+  reviewText: String
+  reviewAuthor: String
+  createdAt: String
+}
+
 type Auth {
     token: ID
     user: User
   }
 
   type Query {
-    properties: [Property]
+    users: [User]
+    user(username: String!): User
+    properties(username: String): [Property]
+    property(propertyId: ID!): Property
+    me: User
   }
 type Mutation {
     login(username: String!, password: String!): Auth
     addUser(firstName: String!, lastName: String!, email: String!, username: String!, password: String!): Auth
     addProperty( propertyType: String!, listingAgent: String!, county: String!, address: String!, zipCode: Int!, price: Int!, bedroomCount: Int!, bathroomCount: Int!, petsAllowed: Boolean!, sqFootage: Int!, depositFee: Int!): Property
+    addReview(propertyId: ID!, reviewAuthor: String!): Property
 }
 `;
 
