c16ed978ea9b5b1143d1aa531b5d7be9e2800e5c
Hailey Flora <Haileycflora@gmail.com>
2023-07-01 15:10:13 -0400

    updated Results.js and RentHome.js Getting filtered search results back FINALLY

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,49 +1,54 @@
-import React, { useState } from 'react';
+import React from "react";
 
-import { useLocation } from 'react-router-dom';
-
+import { useLocation } from "react-router-dom";
 
 const Results = () => {
-
-  const [minPrice, setMinPrice] = useState('');
-  const [maxPrice, setMaxPrice] = useState('');
-  const [bedrooms, setBedrooms] = useState('');
-  const [bathrooms, setBathrooms] = useState('');
-  const [county, setCounty] = useState('');
-
-  //use location from navigate RentHouse page 
+  //use location from navigate RentHouse page
   const location = useLocation();
   console.log(location.state.properties);
 
-  //modify this to filter location.state.properties 
-  const filteredData = properties.filter((property) => {
-    return (
-      property.propertyType === 'House' &&
-      property.price >= minPrice &&
-      property.price <= maxPrice &&
-      property.bedroomCount === bedrooms &&
-      property.bathroomCount === bathrooms &&
-      property.county.toLowerCase() === county.toLowerCase()
-    );
-  });
+  //filters has a state and filters property
+  const filters = location.state.filters;
 
-  // then in return swap location states to filteredData for your map 
-  return (
-    <div className="container my-1">
-      {location.state.length === 0 ? (
-        <p>No results found.</p>
-      ) : (
-        location.state.properties.map((property) => (
+  //getting filtered values by themselves for filteringData
+  const { minPrice, maxPrice, bedrooms, bathrooms, county } = filters;
+
+  //modify this to filter location.state.properties
+  let filteredData = [];
+  if (location.state.properties) {
+    filteredData = location.state.properties.filter((property) => {
+      return (
+        property.propertyType === "House" &&
+        property.price >= minPrice &&
+        property.price <= maxPrice &&
+        property.bedroomCount >= bedrooms &&
+        property.bathroomCount >= bathrooms &&
+        property.county.toLowerCase() === county.toLowerCase()
+      );
+    });
+  }
+
+  // then in return swap location states to filteredData for your map
+  if (filteredData.length === 0) {
+    return (
+      <div className="container my-1">
+        <p>Sorry, There are no available rentals that fit your criteria.</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container my-1">
+        {filteredData.map((property) => (
           <div key={property.id} className="property-container">
             <h3>Price: ${property.price}/mo</h3>
             <p>Bedrooms: {property.bedroomCount}</p>
             <p>Bathrooms: {property.bathroomCount}</p>
             <p>Address: {property.address}</p>
           </div>
-        ))
-      )}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Results;
