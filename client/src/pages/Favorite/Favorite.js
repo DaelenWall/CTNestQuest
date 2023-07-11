66e4a988021cb3eb2f22dc5265b7fbdc50534cde
Hailey Flora <Haileycflora@gmail.com>
2023-07-10 22:22:26 -0400

    CSS for Favorite.js

--- client/src/pages/Favorite/Favorite.js
+++ client/src/pages/Favorite/Favorite.js
@@ -3,18 +3,21 @@ import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../../utils/queries';
 import { Link } from 'react-router-dom';
 
+import "../Favorite/Favorite.css";
+
 function Favorite() {
   const { loading, data } = useQuery(QUERY_ME);
   const me = data?.me;
 
   return (
-    <div className="container my-1">
+    <div className="main-container my-1">
       {me && (
         <>
           {console.log(me)}
           <h2>
             Favorites for {me.firstName} {me.lastName}
           </h2>
+          <div className="favorite-outer-container">
           {me.favorites.map((favorite) => (
           <Link to={`/single-property/${favorite._id}`}>
           <div className="favorite-container">
@@ -25,10 +28,12 @@ function Favorite() {
                 {favorite.bedroomCount} Bedroom {favorite.bathroomCount} Bath
               </h4>
                 <h4>{favorite.address}</h4>
+                <h4>{favorite.propertyType}</h4>
             </div>
           </div>
         </Link>
           ))}
+          </div>
         </>
 
       )}
